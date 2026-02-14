09af1eb38bdfff5cb6a5c54b3c5415d7186803b5
Leonides Fernando <leonides.fernando@gmail.com>
2026-02-14 09:55:01 +0100

    Fixing script to create kafka topics

--- config/dev-local/kafka/create-topics.bat
+++ config/dev-local/kafka/create-topics.bat
@@ -2,7 +2,8 @@ REM # Wait for Kafka to start
 sleep 10
 
 REM # Create Kafka topics
-kafka-topics.sh --create --topic test_software_producer_topic --bootstrap-server localhost:9092 --partitions 3 --replication-factor 1
-kafka-topics.sh --create --topic test_software_consumer_topic --bootstrap-server localhost:9092 --partitions 2 --replication-factor 1
+REM kafka-topics.sh --create --topic test_software_producer_topic --bootstrap-server localhost:9092 --partitions 3 --replication-factor 1
+REM kafka-topics.sh --create --topic test_software_consumer_topic --bootstrap-server localhost:9092 --partitions 2 --replication-factor 1
+kafka-topics.sh --create --topic test_software_incoming_topic --bootstrap-server localhost:9092 --partitions 2 --replication-factor 1
 
 echo "Kafka topics created successfully!"
